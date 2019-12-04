d13c5d2bf6dff72d90162f4c53e126197831e886
Robert Pierce <pierce1673@gmail.com>
2019-12-04 14:02:16 -0500

    updates to index to console log regions + initial attempt at app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,41 @@
-$("path, circle").hover(function(e) {
-  $("#info-box").css("display", "block");
-  $("#info-box").html($(this).data("info"));
+// $(".state>a").on("click", function() {
+//   // console.log("ive been clicked");
+//   console.log($(this).data("region"));
+
+// });
+
+// function getData(id){
+//   const
+//   $.get("/api/regions/:" + id, function(data){
+//     if(data){
+
+//     }
+//   })
+// }
+
+$(".state>a").on("click", function() {
+  console.log($(this).data("region"));
+  $(this).data("region");
+  const item = {
+    id: $(this)
+      .parents("tr")
+      .find(".id")
+      .text(),
+    common_name: $(this)
+      .parents("tr")
+      .find(".common_name")
+      .text(),
+    scientific_name: $(this)
+      .parents("tr")
+      .find(".scientific_name")
+      .text(),
+    image_url: $(this)
+      .parents("tr")
+      .find(".image_url")
+      .text(),
+    description: $(this)
+      .parents("tr")
+      .find(".description")
+      .text()
+  };
 });
-$("path, circle").mouseleave(function(e) {
-  $("#info-box").css("display", "none");
-});
-$(document)
-  .mousemove(function(e) {
-    $("#info-box").css("top", e.pageY - $("#info-box").height() - 30);
-    $("#info-box").css("left", e.pageX - $("#info-box").width() / 2);
-  })
-  .mouseover();
