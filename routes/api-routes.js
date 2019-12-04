008fba4aff4983b3943cf9ec98fa7cd0d74128be
Farrah Carter <carterfarraha@gmail.com>
2019-12-04 12:29:07 -0500

    Added post route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -23,6 +23,27 @@ module.exports = function (app) {
         });
     });
 
+    //Pulls up result for plant
+    app.get("/api/plants/:plant", function (req, res) {
+
+        db.Plant.findOne({
+            where: {
+                id: req.params.plant
+            },
+            include: [{
+                model: db.Region,
+                through: {
+                    where: {
+                        RegionId: req.params.plant,
+                    }
+                }
+            }],
+
+        }).then(function (result) {
+            return res.json(result);
+        });
+    });
+
     // Displays all plants on the page
     app.get("/api/plants", function (req, res) {
 
@@ -33,11 +54,28 @@ module.exports = function (app) {
     });
 
     //Adds plant to the database
-    app.post("/api/add", function (req, res) {
+    app.post("/api/add", async function (req, res) {
+
+        var newPlant = req.body;
+
+        await db.Plant.create({
+            common_name: newPlant.common_name,
+            scientific_name: newPlant.scientific_name,
+            img_url: newPlant.img_url,
+            description: newPlant.description
+        });
+
+        const plantArr = await db.Plant.findAll();
+        const regionArr = await db.Region.findAll();
+        const promises = [];
+        let plantIdx = plantArr.length - 1;
+        let regionIdx = newPlant.region;
+        promises.push(plantArr[plantIdx].addRegion(regionArr[regionIdx]));
+
+        await Promise.all(promises);
+
+        res.status(204).end();
 
     });
-
-
-
 
 }
