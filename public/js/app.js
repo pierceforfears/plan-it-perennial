970d6fbe6ac2b8abb93016aeecb198922a2d37c3
Robert Pierce <pierce1673@gmail.com>
2019-12-04 23:11:48 -0500

    adding render results

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,17 @@
 function getData(id) {
   $.get("/api/regions/" + id, function(data) {
-    console.log(data.Plants);
+    console.table(data.Plants);
+    $(".main-section").empty();
+    let newPlant = data.Plants;
+    let regionPlants = [];
+    for (let i = 0; i < newPlant.length; i++) {
+      newPlant.push(regionPlants[i]);
+      $(".main-section").append(`<img src="../${newPlant[i].img_url}">`);
+      $(".main-section").append(`<h4>${newPlant[i].common_name}</h4>`);
+      $(".main-section").append(`<h5>${newPlant[i].scientific_name}</h5>`);
+      $(".main-section").append(`<p>${newPlant[i].description}</p>`);
+      $(".main-section").append(`<hr/>`);
+    }
   });
 }
 
@@ -14,47 +25,3 @@ if (window.location.pathname === "/display") {
   const regionId = params.get("regionId");
   getData(regionId);
 }
-
-// let region = [];
-// //this is the function to render all items in a database.
-// const renderItems = function(items) {
-//   items.forEach(function(item) {
-//     let newRegion = $(` <tr>
-//           <td class="common_name">${item.common_name}</td>
-//           <td class="scientific_name">${item.scientific_name}</td>
-//           <td class="image_url">${item.image_url}</td>
-//           <td class="description">${item.description}</td>
-//         </tr>`);
-//     $(".tbodypage").append(newRegion);
-//   });
-// };
-
-// $(".state>a").on("click", function() {
-//   console.log($(this).data("region"));
-//   $(this).data("region");
-//   const plant = {
-//     id: $(this)
-//       .parents("tr")
-//       .find(".id")
-//       .text(),
-//     common_name: $(this)
-//       .parents("tr")
-//       .find(".common_name")
-//       .text(),
-//     scientific_name: $(this)
-//       .parents("tr")
-//       .find(".scientific_name")
-//       .text(),
-//     image_url: $(this)
-//       .parents("tr")
-//       .find(".image_url")
-//       .text(),
-//     description: $(this)
-//       .parents("tr")
-//       .find(".description")
-//       .text()
-//   };
-//   console.log(plant);
-
-//   console.table(region);
-// });
